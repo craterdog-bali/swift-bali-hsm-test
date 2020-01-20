3f7dcb8d1976be82615045137932f950f7fcf7ff
Derk Norton <derk.norton@gmail.com>
2020-01-20 14:55:38 -0700

    Added checks in switch statement to handle the retry cases.

--- ArmorDTest/AppDelegate.swift
+++ ArmorDTest/AppDelegate.swift
@@ -44,12 +44,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     class FlowController: FlowControl {
         var step = 0
         var account = formatter.generateTag()
-        var bytesLess = formatter.generateBytes(size: 510 - 34 - 3)   // one byte less than a block
-        var bytesEqual = formatter.generateBytes(size: 510 - 34 - 2)  // exactly one block
-        var bytesMore = formatter.generateBytes(size: 510 - 34 - 1)   // one byte more than a block
-        var bytesLong = formatter.generateBytes(size: 1024)
         var mobileKey = formatter.generateBytes(size: 32)
-        var publicKey: [UInt8]?
         var credentials: Document?
         var certificate: Document?
         var certificateCitation: Citation?
@@ -72,65 +67,64 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     print("Key erased: \(String(describing: result))")
                     device.processRequest(type: "generateKeys", mobileKey)
                 case 3:
-                    print("Key generated: \(String(describing: result))")
-                    publicKey = result!
-                    let content = Certificate(publicKey: publicKey!)
-                    certificate = Document(account: account, content: content)
+                    if result != nil {
+                        print("Key generated: \(String(describing: result))")
+                        let content = Certificate(publicKey: result!)
+                        certificate = Document(account: account, content: content)
+                    }
                     let bytes = [UInt8](certificate!.format().utf8)
                     device.processRequest(type: "signBytes", mobileKey, bytes)
                 case 4:
-                    print("Certificate signed: \(String(describing: result))")
-                    let content = certificate!.content
-                    let signature = result!
-                    certificate = Document(account: account, content: content, signature: signature)
+                    if result != nil {
+                        print("Certificate signed: \(String(describing: result))")
+                        let content = certificate!.content
+                        certificate = Document(account: account, content: content, signature: result!)
+                    }
                     let bytes = [UInt8](certificate!.format().utf8)
                     device.processRequest(type: "digestBytes", bytes)
                 case 5:
-                    print("Certificate digested: \(String(describing: result))")
-                    var content = certificate!.content
-                    let tag = content.tag
-                    let version = content.version
-                    let digest = result!
-                    certificateCitation = Citation(tag: tag, version: version, digest: digest)
-                    content = Credentials()
-                    credentials = Document(account: account, content: content, certificate: certificateCitation)
+                    if result != nil {
+                        print("Certificate digested: \(String(describing: result))")
+                        let tag = certificate!.content.tag
+                        let version = certificate!.content.version
+                        certificateCitation = Citation(tag: tag, version: version, digest: result!)
+                        let content = Credentials()
+                        credentials = Document(account: account, content: content, certificate: certificateCitation)
+                    }
                     let bytes = [UInt8](credentials!.format().utf8)
                     device.processRequest(type: "signBytes", mobileKey, bytes)
                 case 6:
-                    print("Credentials signed: \(String(describing: result))")
-                    let bytes = [UInt8](credentials!.format().utf8)
-                    let content = credentials!.content
-                    let signature = result!
-                    credentials = Document(account: account, content: content, certificate: certificateCitation, signature: signature)
-                    device.processRequest(type: "validSignature", publicKey!, signature, bytes)
-                case 7:
-                    print("Credentials validated: \(String(describing: result))")
-                    repository.writeDocument(credentials: credentials!, document: certificate!)
-                    var content = certificate!.content
-                    let name = "/bali/examples/certificate"
-                    let version = content.version
-                    repository.writeCitation(credentials: credentials!, name: name, version: version, citation: certificateCitation!)
-                    content = Transaction(merchant: "Starbucks", amount: "$4.95")
-                    transaction = Document(account: account, content: content, certificate: certificateCitation)
+                    if result != nil {
+                        print("Credentials signed: \(String(describing: result))")
+                        var content = credentials!.content
+                        credentials = Document(account: account, content: content, certificate: certificateCitation, signature: result!)
+                        repository.writeDocument(credentials: credentials!, document: certificate!)
+                        let name = "/bali/examples/certificate"
+                        let version = certificate!.content.version
+                        repository.writeCitation(credentials: credentials!, name: name, version: version, citation: certificateCitation!)
+                        content = Transaction(merchant: "Starbucks", amount: "$4.95")
+                        transaction = Document(account: account, content: content, certificate: certificateCitation)
+                    }
                     let bytes = [UInt8](transaction!.format().utf8)
                     device.processRequest(type: "signBytes", mobileKey, bytes)
-                case 8:
-                    print("Transaction signed: \(String(describing: result))")
-                    let content = transaction!.content
-                    let signature = result!
-                    transaction = Document(account: account, content: content, certificate: certificateCitation, signature: signature)
+                case 7:
+                    if result != nil {
+                        print("Transaction signed: \(String(describing: result))")
+                        let content = transaction!.content
+                        transaction = Document(account: account, content: content, certificate: certificateCitation, signature: result!)
+                        repository.writeDocument(credentials: credentials!, document: transaction!)
+                    }
                     let bytes = [UInt8](transaction!.format().utf8)
                     device.processRequest(type: "digestBytes", bytes)
-                case 9:
-                    print("Transaction digested: \(String(describing: result))")
-                    let content = transaction!.content
-                    let tag = content.tag
-                    let version = content.version
-                    let digest = result!
-                    let citation = Citation(tag: tag, version: version, digest: digest)
-                    repository.writeDocument(credentials: credentials!, document: transaction!)
-                    let name = "/bali/examples/transaction"
-                    repository.writeCitation(credentials: credentials!, name: name, version: version, citation: citation)
+                case 8:
+                    if result != nil {
+                        print("Transaction digested: \(String(describing: result))")
+                        let name = "/bali/examples/transaction"
+                        let tag = transaction!.content.tag
+                        let version = transaction!.content.version
+                        let citation = Citation(tag: tag, version: version, digest: result!)
+                        repository.writeCitation(credentials: credentials!, name: name, version: version, citation: citation)
+                    }
                     device.processRequest(type: "eraseKeys")
                 default:
                     return  // done
